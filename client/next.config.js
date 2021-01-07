d1c75add300c9367b92b5f5efa09979556a62822
Gerardo Villa <gera.villa21@gmail.com>
2021-01-06 21:56:30 -0600

    add environment variables & add images domains

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,19 @@
 const path = require("path");
-const nextEnv = require("next-env");
+// const nextEnv = require("next-env");
 
-const withNextEnv = nextEnv();
+// const withNextEnv = nextEnv();
 
-module.exports = withNextEnv({
+module.exports = {
+  env: {
+    API_HOST_CONTAINER: process.env.API_HOST_CONTAINER,
+    API_HOST_PUBLIC: process.env.API_HOST_PUBLIC,
+    IMAGES_DOMAIN: process.env.IMAGES_DOMAIN,
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-});
+
+  images: {
+    domains: [process.env.IMAGES_DOMAIN]
+  }
+};
